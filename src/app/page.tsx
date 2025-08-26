87fd8c68dc51ea55f271d0d533c18452af291482
Ooozari <uzairasiff1227@gmail.com>
2025-08-26 15:36:50 +0500

    practise: Union Interstion of types aliases

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
     isActive: boolean;
   }
   const [user, setUser] = useState<User | null>(null)
+  const [show, setShow] = useState<boolean>(false)
 
   function createUser(user: User): void {
     setUser(user);
@@ -22,20 +23,57 @@ export default function Home() {
 
   }
 
-  // const createdUser = createUser(userDetails)
-  // console.log(createdUser)
+  // Union Interstion of types aliases
+  type Employee = {
+    readonly _id: number,
+    name: string,
+    hour: number,
+  }
+
+  type Doctor = {
+    sector?: string,
+    specilization: string,
+  }
+
+  type Sergon = Employee & Doctor & {
+    ward: string,
+  }
+
+  const sergon1: Sergon = {
+    _id: 12345,
+    name: "guest",
+    hour: 3,
+    sector: "Lahore",
+    specilization: "Dentist",
+    ward: "Health",
+
+  }
+
+
+
   return (
     <div className="font-sans flex flex-col min-h-screen p-8 pb-20 gap-8 sm:p-20">
       <h1>TypeScipt Mastery | TS</h1>
 
 
-      {/* Section 1 : */}
+      {/* Section 1 : type aliase*/}
       <div>
         <h1>Displaying object using type aliases to decalare the user Object</h1>
         <button onClick={() => createUser(userDetails)}>Create User</button>
         {user && <div>{JSON.stringify(user)}</div>}
 
       </div>
+
+      <div>
+        <h1>Union Interstion of types aliases</h1>
+        <button onClick={() => setShow(!show)}>Show Sergon details</button>
+        {show && <div>
+          {JSON.stringify(sergon1)}
+        </div>
+        }
+      </div>
+
     </div>
+
   );
 }
